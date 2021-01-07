655a8c269102faf8132b644da7b6c563ab7288bd
Miguel <miguel.moreno@dataleanmakers.es>
2021-01-07 14:40:21 +0100

    menu sidebar creado

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include "_share/header.php"; ?>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        <?php include "_share/topbar.php" ?>
+        <?php include "_share/sidebar.php" ?>
+    </div>
+</body>
