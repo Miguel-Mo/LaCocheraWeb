026530bad002815a2a3e55e82c72a53504faaa3b
Miguel <miguel.moreno@dataleanmakers.es>
2021-01-15 11:04:50 +0100

    icons

--- _share/header.php
+++ _share/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>La Cochera Web</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -27,5 +27,5 @@
   <!-- summernote -->
   <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
   <!-- favicon -->
-  <link rel="icon" type="image/png" href="dist\img\AdminLTELogo.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="..\dist\img\AdminLTELogo.png" sizes="16x16">
 </head>
