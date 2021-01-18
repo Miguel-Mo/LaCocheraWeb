f4da40de319b953f43ff666b41b6c1396fa5d7b4
Miguel <miguel.moreno@dataleanmakers.es>
2021-01-18 21:27:42 +0100

    modal modif

--- content/modales/detallesPropuestaVenta.php
+++ content/modales/detallesPropuestaVenta.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-lg" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -10,7 +10,12 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="card card-info">
+                    <!-- datos de la propuesta -->
+
+                    <div class="card col-12">
+                        <div class="card-header">
+                            Datos de la propuesta
+                        </div>
                         <div class="col">
                             <label>Vendedor: </label>
                         </div>
@@ -18,159 +23,143 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-
-                    <div class="card card-info">
                         <div class="col">
                             <label>Presupuesto: </label>
                         </div>
                         <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-euro-sign"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
 
-                    <div class="card card-info">
                         <div class="col">
                             <label>Fecha de Venta: </label>
                         </div>
                         <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
                     </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+
+                    <!-- datos del vehiculo  -->
+                    <div class="card col-12">
+                        <div class="card-header">
+                            Datos del vehiculo
+                        </div>
+                        <div class="col">
+                            <label>Vehículo: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-car"></i></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+
+                        <div class="col">
+                            <label>Tipo de vehículo: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-parking"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+
+                        <div class="col">
+                            <label>Matrícula: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-barcode"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+                        <div class="col">
+                            <label>Concesionario: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-map-marker"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
                     </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+                    <!-- datos del cliente -->
+                    <div class="card col-12">
+                        <div class="card-header">
+                            Datos del cliente
+                        </div>
+                        <div class="col">
+                            <label>Cliente: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+
+                        <div class="col">
+                            <label>E-mail: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-euro-sign"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+
+                        <div class="col">
+                            <label>Teléfono: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-phone"></i></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+                        <div class="col">
+                            <label>Fecha de Registro: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-calendar-plus"></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
+                        <div class="col">
+                            <label>DNI: </label>
+                        </div>
+                        <div class="col input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                <span class="input-group-text"><i class="fas fa-id-card"></i></i></span>
                             </div>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input type="email" class="form-control">
                         </div>
 
                     </div>
                 </div>
-                <div class="row">
-                    <div class="card card-info">
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
-                            </div>
-                            <input type="email" class="form-control" placeholder="Email">
-                        </div>
-
-                    </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary">Aceptar</button>
                 </div>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                <button type="button" class="btn btn-primary">Aceptar</button>
             </div>
         </div>
     </div>
-</div>
